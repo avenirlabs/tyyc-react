7416fe63c173cbe7c90c281ee26dde31be81786f
Avenir Labs <a@avenirlabs.com>
2025-08-03 15:32:35 +0530

    Update categories.ts

--- src/pages/api/categories.ts
+++ src/pages/api/categories.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import api from '@/lib/woocommerce';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(_, res: NextApiResponse) {
   try {
     const response = await api.get('products/categories');
     res.status(200).json(response.data);
