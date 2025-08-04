faf2293c73774adbed6b8e4013923db149d7f65e
Avenir Labs <a@avenirlabs.com>
2025-08-04 15:12:52 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 
 export default function HomePage() {
-  const [loading, setLoading] = useState(true);
+  const [_, setLoading] = useState(true);
 
   const fetchProducts = async (categoryId?: number) => {
     setLoading(true);
